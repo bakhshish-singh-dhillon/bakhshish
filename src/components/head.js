f253c3891a34225b893dc0c10df4cde028fe3b10
Bakhshish Singh Dhillon <bakhshish.dev@gmail.com>
2021-10-12 00:32:01 -0500

    Social Media Pic Fix

--- src/components/head.js
+++ src/components/head.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import profilePic from '../img/Bakhshish-Singh-Dhillon.png'
-import twitterPic from '../img/Bakhshish-Singh-Dhillon-t.png'
+import socialMediaPic from '../img/Bakhshish-Singh-Dhillon-t.png'
 
 const Head = ({ title }) => {
     return (
@@ -12,13 +12,13 @@ const Head = ({ title }) => {
                 <meta name="keyword" content="Bakhshish Singh Dhillon, portfolio, javascript, web developer, programmer, php, embedded systems, smart watch, react, laravel" />
                 <meta property="og:title" content="Bakhshish Singh Dhillon | Backend Web Developer"/>
                 <meta property="og:description" content="Bakhshish Singh Dhillon Web Developer" />
-                <meta property="og:image" content={`https://www.bakhshishsingh.com${profilePic}`}/>
+                <meta property="og:image" content={`https://www.bakhshishsingh.com${socialMediaPic}`}/>
                 <meta property="og:url" content="https://www.bakhshishsingh.com" />
                 <meta property="og:type" content="website" />
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content="Bakhshish Singh Dhillon | Backend Web Developer" />
                 <meta name="twitter:description" content="Bakhshish Singh Dhillon Web Developer"/>
-                <meta name="twitter:image" content={`https://www.bakhshishsingh.com${twitterPic}`}/>
+                <meta name="twitter:image" content={`https://www.bakhshishsingh.com${socialMediaPic}`}/>
                 {/* <meta name="twitter:image" content={`https://www.bakhshishsingh.com${profilePic}`}/> */}
                 {/* <link rel="shortcut icon" href="./assets/favicon.ico" type="image/x-icon" />  */}
             </Helmet>
