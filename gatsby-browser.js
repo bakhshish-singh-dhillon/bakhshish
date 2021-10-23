d25c854ea78c9b592bca0dca2767dba48fcfe04e
Bakhshish Singh Dhillon <bakhshish.dev@gmail.com>
2021-10-23 11:05:36 -0500

    Bootstrap and google analytics added

---
+++ gatsby-browser.js
@@ -0,0 +1,4 @@
+import './src/scss/index.scss'
+import 'jquery/dist/jquery.min.js'
+import '@popperjs/core/dist/umd/popper.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,34 @@ module.exports = {
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-plugin-fontawesome-css',
-    'gatsby-plugin-react-helmet'
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-NFPMRX6RR0', // Google Analytics / GA
+          // optional
+          //'OPTIONAL----AW-CONVERSION_ID', // Google Ads / Adwords / AW
+          //'OPTIONAL----DC-FLOODIGHT_ID', // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          //optimize_id: 'OPT_CONTAINER_ID',
+          //anonymize_ip: true,
+          //cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          //respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          //exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
+      },
+    },
   ],
 }
